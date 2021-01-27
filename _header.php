2a1a9f0c9939b0749b167e86416651c09c98381c
caroww <caroline.ww@wanadoo.fr>
2021-01-27 08:54:29 +0100

    hackathon2

---
+++ _header.php
@@ -0,0 +1,15 @@
+<div class="container nav fixed-top justify-content-center">
+    <div class="header_section_top">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="custom_menu">
+                    <ul>
+                        <li><a href="index.php">Les Supers</a></li>
+                        <li><a href="about-us.php">À Propos</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
